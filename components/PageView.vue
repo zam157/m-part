bb046aa2751a759b99fb0d4aff911ddc5bb517df
Zam <24277775+zam157@users.noreply.github.com>
2023-08-13 13:40:34 +0800

    Remove sample code

--- components/PageView.vue
+++ components/PageView.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/pageview')
-
-const time = useTimeAgo(() => data.value?.startAt || 0)
 </script>
 
 <template>
   <div text-gray:80>
-    <span font-500 text-gray>{{ data?.pageview }}</span>
-    page views since
-    <span text-gray>{{ time }}</span>
+    Hello World!
   </div>
 </template>
