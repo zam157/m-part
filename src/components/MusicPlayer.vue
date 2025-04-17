63582d0d07535fafdb614206d09b87409ef8270b
Zam <24277775+zam157@users.noreply.github.com>
2025-04-17 21:19:32 +0800

    init

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+const audioRef = ref<HTMLAudioElement>()
+const { playing, duration, currentTime, volume } = useMediaControls(audioRef, { src: 'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3' })
+const progress = computed(() => duration.value === 0 ? 0 : currentTime.value / duration.value)
+
+const scrubbing = ref(false)
+// Pause the music when scrubbing
+let isPlayingBeforeScrub = false
+watch(scrubbing, (value) => {
+  if (value) {
+    isPlayingBeforeScrub = playing.value
+    playing.value = false
+  }
+  else {
+    playing.value = isPlayingBeforeScrub
+  }
+})
+
+const progressBarRef = ref<HTMLDivElement>()
+useEventListener('mouseup', () => scrubbing.value = false)
+const { elementX, elementWidth } = useMouseInElement(progressBarRef)
+watchEffect(() => {
+  if (!scrubbing.value)
+    return
+  const _elementX = elementX.value < 0
+    ? 0
+    : elementX.value > elementWidth.value
+      ? elementWidth.value
+      : elementX.value
+  const currentProgress = _elementX / elementWidth.value
+  currentTime.value = currentProgress * duration.value
+})
+</script>
+
+<template>
+  <div fixed inset-x-0 bottom-0 z-50 flex="~ col" select-none>
+    <!-- Process bar -->
+    <div
+      ref="progressBarRef"
+      h="4px" hover="h-[6px] cursor-pointer"
+      flex justify-start transition-height
+      @mousedown="scrubbing = true"
+    >
+      <div
+        h-full w-full transform-origin-left bg-blue
+        :style="{ transform: `scaleX(${progress})` }"
+      />
+    </div>
+
+    <!-- Music player -->
+    <div h-19 flex>
+      <audio ref="audioRef" />
+      <!-- Music image -->
+      <div aspect-1 h-full bg-blue-gray />
+
+      <!-- Music info -->
+      <div flex="~ col 1" items-start justify-center gap-2 px-4>
+        <!-- Song name -->
+        <span text-black font-bold dark:text-white>Chaff & Dust</span>
+        <!-- Artist name -->
+        <span text-xs text-gray:80>
+          HYONNA
+        </span>
+      </div>
+
+      <!-- Volume -->
+      <div flex="~" items-center justify-center gap-2 px-2 font-size-6>
+        <input v-model="volume" type="range" min="0" max="1" step="0.01">
+      </div>
+
+      <!-- Play button -->
+      <div class="btn-wrapper" flex="~" items-center justify-between gap-2 px-2 font-size-6>
+        <span class="i-material-symbols:skip-previous-rounded" />
+        <span :class="playing ? 'i-material-symbols:pause-rounded' : 'i-material-symbols:play-arrow-rounded'" @click="playing = !playing" />
+        <span class="i-material-symbols:skip-next-rounded" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btn-wrapper > span {
+  @apply hover:text-gray/90 active:text-light/90;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const { t } = useI18n()
+useHead({
+  title: () => t('button.home'),
+})
+</script>
+
+<template>
+  <div>
+    <MusicPlayer />
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
