a9d124603a24455e8f9f4a9a47e8ae585249a8f6
Zam <24277775+zam157@users.noreply.github.com>
2025-12-22 03:36:14 +0800

    refactor: remove deprecated `isClient` value

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -3,13 +3,13 @@ import { createPinia } from 'pinia'
 
 // Setup Pinia
 // https://pinia.vuejs.org/
-export const install: UserModule = ({ isClient, initialState, app }) => {
+export const install: UserModule = ({ initialState, app }) => {
   const pinia = createPinia()
   app.use(pinia)
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
   // for other serialization strategies.
-  if (isClient)
+  if (!import.meta.env.SSR)
     pinia.state.value = (initialState.pinia) || {}
 
   else
